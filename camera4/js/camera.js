49ad5d9a4c7954b1d25318bf1ee0a64dce415ad6
richard-shepherd <richard.s.shepherd@gmail.com>
2016-12-06 20:01:24 +0000

    x

---
+++ camera4/js/camera.js
@@ -0,0 +1,44 @@
+/**
+ * @constructor
+ */
+function Camera(width, height, facingDirection, videoElement) {
+
+    // The video element we show video in...
+    this.videoElement = videoElement;
+
+    // We create the video part of the constraints...
+    var videoConstraints = {
+        width : width,
+        height: height
+    };
+    if(facingDirection === Camera.Direction.FRONT_FACING) {
+        videoConstraints.facingMode = "user";
+    }
+    if(facingDirection === Camera.Direction.BACK_FACING) {
+        videoConstraints.facingMode = "environment";
+    }
+
+    // We create the constraints...
+    var constraints = {audio: false, video: videoConstraints };
+
+    // We connect to the video stream from the camera...
+    var that = this;
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) { that.gotVideoStream(stream); });
+}
+
+/**
+ * An enum for the direction cameras can face (particularly in phones).
+ */
+Camera.Direction = {
+    DEFAULT : 0,
+    FRONT_FACING : 1,
+    BACK_FACING : 2
+};
+
+/**
+ * Called when we have got a video stream.
+ */
+Camera.prototype.gotVideoStream = function (stream) {
+    this.videoElement.src = window.URL.createObjectURL(stream);
+};
