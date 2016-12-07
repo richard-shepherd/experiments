e0d6809318fbbd2b7e47a09af3c4520f9f160571
richard-shepherd <richard.s.shepherd@gmail.com>
2016-12-07 20:36:45 +0000

    x

--- camera4/js/camera.js
+++ camera4/js/camera.js
@@ -6,6 +6,7 @@
  * @constructor
  */
 function Camera(width, height, facingDirection, videoElement) {
+    Logger.log("Starting camera");
 
     // We keep hold of the construction parameters...
     this.width = width;
@@ -17,35 +18,17 @@ function Camera(width, height, facingDirection, videoElement) {
     // This is a collection of objects with 'label' and 'deviceID' fields.
     this.cameras = [];
 
-    // The index into the cameras collection for the preferred camera
-    // (which matches the facingDirection requested)...
+    // The index into the cameras collection for the preferred camera (which matches the facingDirection requested)...
     this.preferredCameraIndex = 0;
 
-    Logger.log("Starting camera");
+    // The current camera index...
+    this.currentCameraIndex = 0;
+
+    // The stream from the currently active camera...
+    this.activeStream = null;
 
     // We find the available cameras...
-    this.findCameras();
-
-    // // We create the video part of the constraints...
-    // var videoConstraints = {
-    //     width : width,
-    //     height: height,
-    //     facingMode: "environment"
-    // };
-    // if(facingDirection === Camera.FacingDirection.FRONT_FACING) {
-    //     videoConstraints.facingMode = "user";
-    // }
-    // if(facingDirection === Camera.FacingDirection.BACK_FACING) {
-    //     videoConstraints.facingMode = "environment";
-    // }
-
-    // // We create the constraints...
-    // var constraints = {audio: false, video: videoConstraints };
-    //
-    // // We connect to the video stream from the camera...
-    // var that = this;
-    // navigator.mediaDevices.getUserMedia(constraints)
-    //     .then(function(stream) { that.gotVideoStream(stream); });
+    this._findCameras();
 }
 
 /**
@@ -58,11 +41,25 @@ Camera.FacingDirection = {
 };
 
 /**
- * findCameras
- * -----------
+ * toggleCamera
+ * ------------
+ * Switches between the available cameras.
+ */
+Camera.prototype.toggleCamera = function() {
+    Logger.log("Toggling camera");
+    this.currentCameraIndex++;
+    if(this.currentCameraIndex >= this.cameras.length) {
+        this.currentCameraIndex = 0;
+    }
+    this._selectCamera();
+};
+
+/**
+ * _findCameras
+ * ------------
  * Finds the collection of available cameras.
  */
-Camera.prototype.findCameras = function() {
+Camera.prototype._findCameras = function() {
     Logger.log("Finding available cameras");
 
     // We clear any existing camera info we hold...
@@ -71,16 +68,16 @@ Camera.prototype.findCameras = function() {
     // We find the collection of cameras...
     var that = this;
     navigator.mediaDevices.enumerateDevices()
-        .then(function(deviceInfos) { that.gotCameras(deviceInfos); })
+        .then(function(deviceInfos) { that._gotCameras(deviceInfos); })
         .catch(Logger.log);
 };
 
 /**
- * gotCameras
- * ----------
+ * _gotCameras
+ * -----------
  * Called when we have found the collection of cameras (and other devices).
  */
-Camera.prototype.gotCameras = function(deviceInfos) {
+Camera.prototype._gotCameras = function(deviceInfos) {
     Logger.log("Got camera infos");
 
     // We find the cameras, and add info about them
@@ -99,15 +96,69 @@ Camera.prototype.gotCameras = function(deviceInfos) {
     for(var i=0; i<this.cameras.length; ++i) {
         var label = this.cameras[i].label;
         if(this.facingDirection === Camera.FacingDirection.FRONT_FACING && label.includes("front")) {
-
+            this.preferredCameraIndex = i;
+            break;
+        }
+        if(this.facingDirection === Camera.FacingDirection.BACK_FACING && label.includes("back")) {
+            this.preferredCameraIndex = i;
+            break;
         }
     }
+
+    // We select the camera...
+    this.currentCameraIndex = this.preferredCameraIndex;
+    this._selectCamera();
 };
 
 /**
+ * _selectCamera
+ * -------------
+ * Selects the camera with the current camera index.
+ */
+Camera.prototype._selectCamera = function() {
+    // We stop the current stream before starting a new one...
+    this._stopActiveStream();
+
+    // We find the device ID of the current camera...
+    var cameraInfo = this.cameras[this.currentCameraIndex];
+    Logger.log("Connecting to camera: " + cameraInfo.label);
+
+    // We create the constraints...
+    var videoConstraints = {
+        width : this.width,
+        height: this.height,
+        deviceId: {exact: cameraInfo.deviceID }
+    };
+    var constraints = {audio: false, video: videoConstraints };
+
+    // We connect to the video stream from the camera...
+    var that = this;
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) { that._gotVideoStream(stream); });
+};
+
+/**
+ * _gotVideoStream
+ * ---------------
  * Called when we have got a video stream.
  */
-Camera.prototype.gotVideoStream = function (stream) {
+Camera.prototype._gotVideoStream = function (stream) {
     Logger.log("Got video stream");
+    this.activeStream = stream;
     this.videoElement.src = window.URL.createObjectURL(stream);
+};
+
+/**
+ * _stopActiveStream
+ * -----------------
+ * Stops the active stream, if there is one.
+ */
+Camera.prototype._stopActiveStream = function() {
+    Logger.log("Stopping active stream");
+    if (this.activeStream) {
+        this.activeStream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+    this.activeStream = null;
 };
