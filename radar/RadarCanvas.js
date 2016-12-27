cfb368bdbdfa31f5e9afa8ea2012681658ca0c93
richard-shepherd <richard.s.shepherd@gmail.com>
2016-12-27 17:28:08 +0000

    x

--- radar/RadarCanvas.js
+++ radar/RadarCanvas.js
@@ -14,12 +14,16 @@ function RadarCanvas(canvasElementID) {
     // The canvas size...
     this._canvasWidth = 1.0;
     this._canvasHeight = 1.0;
+    this._radarRadius = 0.5;
 
     // The most recent compass heading...
     this._compassHeadingRadians = 0.0;
 
+    // The angle of the radar line...
+    this._radarLineAngleRadians = 0.0;
+
     // We set the background color of the canvas...
-    this._canvasElement.style.backgroundColor = "#002000";
+    this._canvasElement.style.backgroundColor = "#000000";
 }
 
 /**
@@ -29,25 +33,70 @@ function RadarCanvas(canvasElementID) {
  */
 RadarCanvas.prototype.showRadar = function(compassHeadingRadians) {
     try {
+        var ctx = this._canvasContext;
+
         // We find the current size of the canvas...
-        this._canvasWidth = this._canvasContext.canvas.width;
-        this._canvasHeight = this._canvasContext.canvas.height;
+        this._canvasWidth = ctx.canvas.width;
+        this._canvasHeight = ctx.canvas.height;
+        this._radarRadius = Math.min(this._canvasWidth, this._canvasHeight);
+        this._radarRadius /= 2.0;
+        this._radarRadius *= 0.85;
 
         // We store the context and restore it later, as we will
         // be applying various transformations to it, such as rotations.
         // (If we don't save and restore, these will be cumulative in
-        // later updates.)
-        this._canvasContext.save();
+        // later updates.)this._radarRadius
+        ctx.save();
 
         // We clear the canvas, before redrawing...
-        this._canvasContext.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
+        ctx.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
+
+        // We set the origin to the center of the canvas...
+        ctx.translate(this._canvasWidth/2, this._canvasWidth/2);
+
+        // We rotate the canvas, so that north points north...
+        ctx.rotate(-1.0 * this._compassHeadingRadians);
+
+        // We show the sweeping, green radar line...
+        this._drawRadarLine();
 
         // We show the compass...
         this._compassHeadingRadians = compassHeadingRadians;
         this._drawCompass();
 
+        this._radarLineAngleRadians += 0.02;
+        if(this._radarLineAngleRadians > 2.0 * Math.PI) {
+            this._radarLineAngleRadians = 0.0;
+        }
+
     } finally {
         this._canvasContext.restore();
+    }
+};
+
+/**
+ * _drawRadarLine
+ * --------------
+ * Draws the sweeping, green radar line.
+ */
+RadarCanvas.prototype._drawRadarLine = function() {
+    var ctx = this._canvasContext;
+
+    var numBands = 400;
+    var greenOffset = 255 / (numBands + 1);
+    var g = 255;
+    var bandWidthRadians = 0.01;
+    var angle = this._radarLineAngleRadians;
+    for(var i=0; i<numBands; ++i) {
+        ctx.fillStyle = Utils.rgbToString(0, g, 0);
+        ctx.beginPath();
+        ctx.arc(0, 0, this._radarRadius, angle, angle-bandWidthRadians, true);
+        ctx.lineTo(0, 0);
+        ctx.closePath();
+        ctx.fill();
+
+        g -= greenOffset;
+        angle -= bandWidthRadians;
     }
 };
 
@@ -57,49 +106,40 @@ RadarCanvas.prototype.showRadar = function(compassHeadingRadians) {
  * Draws the compass.
  */
 RadarCanvas.prototype._drawCompass = function() {
-    // We set the origin to the center of the canvas...
-    this._canvasContext.translate(this._canvasWidth/2, this._canvasWidth/2);
-
-    // We rotate the canvas, so that north points north...
-    this._canvasContext.rotate(-1.0 * this._compassHeadingRadians);
-
-    // We work out the offset of the compass lines from the center...
-    var compassLineOffset = Math.min(this._canvasWidth, this._canvasHeight);
-    compassLineOffset /= 2.0;
-    compassLineOffset *= 0.85;
+    var ctx = this._canvasContext;
 
     // We draw the compass lines...
-    this._canvasContext.beginPath();
-    this._canvasContext.strokeStyle = 'white';
-    this._canvasContext.lineWidth = 2;
-    this._canvasContext.moveTo(0, -compassLineOffset);
-    this._canvasContext.lineTo(0, compassLineOffset);
-    this._canvasContext.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, -1.0 * this._radarRadius);
+    ctx.lineTo(0, this._radarRadius);
+    ctx.stroke();
 
-    this._canvasContext.beginPath();
-    this._canvasContext.strokeStyle = 'white';
-    this._canvasContext.lineWidth = 2;
-    this._canvasContext.moveTo(-compassLineOffset, 0);
-    this._canvasContext.lineTo(compassLineOffset, 0);
-    this._canvasContext.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.moveTo(-1.0 * this._radarRadius, 0);
+    ctx.lineTo(this._radarRadius, 0);
+    ctx.stroke();
 
     // We show the N, E, W, S labels...
     var fontSize = Math.floor(this._canvasWidth / 14.0);
     var fontOffset = fontSize / 2.0 * 0.8;
-    this._canvasContext.font =  fontSize +  "px Arial";
-    this._canvasContext.textAlign = "center";
+    ctx.font =  fontSize +  "px Arial";
+    ctx.textAlign = "center";
 
-    this._canvasContext.fillStyle = "red";
-    this._canvasContext.fillText("N", 0, -compassLineOffset);
+    ctx.fillStyle = "red";
+    ctx.fillText("N", 0, -1.0 * this._radarRadius);
 
-    this._canvasContext.fillStyle = "white";
-    this._canvasContext.fillText("S", 0, compassLineOffset + fontOffset*2);
-    this._canvasContext.fillText("W", -compassLineOffset - fontOffset, fontOffset);
-    this._canvasContext.fillText("E", compassLineOffset + fontOffset, fontOffset);
+    ctx.fillStyle = "white";
+    ctx.fillText("S", 0, this._radarRadius + fontOffset*2);
+    ctx.fillText("W", -1.0 * this._radarRadius - fontOffset, fontOffset);
+    ctx.fillText("E", this._radarRadius + fontOffset, fontOffset);
 
     var shotgun = imageManager.getImage(ImageManager.Image.RADAR_SHOTGUN);
     var imageWidth = 70;
     var imageScale = imageWidth / shotgun.width;
     var imageHeight = shotgun.height * imageScale;
-    this._canvasContext.drawImage(shotgun, 30, 40, imageWidth, imageHeight);
+    ctx.drawImage(shotgun, 30, 40, imageWidth, imageHeight);
 };
